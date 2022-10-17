6eb0d92c53d472d2bfcd1a91d5a6efc116a7ad67
Toba-dean <tobaogundimu@gmail.com>
2022-10-17 05:17:18 +0100

    connect to db successful

--- server/server.js
+++ server/server.js
@@ -5,20 +5,32 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
 
-const userRoute = require("./routes/userRoutes")
+const connectDB = require('./db/connect');
+const userRoute = require("./routes/userRoutes");
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
+app.use(fileUpload({
+  useTempFiles: true,
+}));
 
 
 app.use('/api/v1/user', userRoute)
 
 
-const start = () => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`)
-  })
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port: ${PORT}`);
+    });
+  } catch (error) { 
+    console.log(error.message);
+  }
 }
 
 start()
