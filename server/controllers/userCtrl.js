14e04b115648b74fc8c532065c3e0e808bec1577
Toba-dean <tobaogundimu@gmail.com>
2022-10-17 06:12:58 +0100

    added some errors

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -1,6 +1,25 @@
+const User = require('../models/userModel');
+const { StatusCodes } = require('http-status-codes');
+const {
+  BadRequestError,
+  UnauthenticatedError,
+  NotFoundError
+} = require('../errors')
+
 const userCtrl = {
-  getUser: (req, res) => {
-    res.status(200).json({success: true, msg: "Getting user from the controller."})
+  registerUser: async (req, res) => {
+    const { name, email, password } = req.body
+    if (!name || !email || !password) {
+      throw new BadRequestError("All fields must be filled.")
+    }
+
+    const user = User.findOne({ email })
+    if (user) {
+
+    }
+  },
+  getUser: async (req, res) => {
+    res.status(200).json({ success: true, msg: "Getting user from the controller." })
   }
 }
 
